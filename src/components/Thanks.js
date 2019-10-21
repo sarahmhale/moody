4dd27bd344c8309d3c021f244f26ccbf626804e8
Sarah <sarahhale95@gmail.com>
2019-10-21 10:41:01 +0200

    delete state updates correctlu

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -1,18 +1,39 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 import Emoji from '../shared/components/Emoji'
 
-const Thanks = () => {
-    return (
-        <div className="test-info-container">
-            <div />
-            <div className="data-container">
-                <h1 style={{ color: "#FFFFFF" }}> Thank you for helping with my masterthesis! <Emoji symbol="😍" label="" /></h1>
-            </div>
-            <div />
-        </div>)
-
+class Thanks extends Component {
+    componentDidMount() {
+        this.props.submit(this.props.reviews)
+    }
+    render() {
+        return (
+            <div className="test-info-container">
+                <div />
+                <div className="data-container">
+                    <h1 style={{ color: "#FFFFFF" }}> Thank you for helping with my masterthesis! <Emoji symbol="😍" label="" /></h1>
+                </div>
+                <div />
+            </div>)
+    }
 
 }
 
-export default Thanks;
+
+
+const mapStateToProps = state => {
+    const { data } = state
+    return { reviews: data }
+}
+const mapDispatchToProps = dispatch => {
+    return {
+
+
+        submit: reviews => dispatch(actions.submitReview(reviews))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Thanks);
+
+
