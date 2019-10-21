c37854f15ee4b2420d568788ebcb7f3ef6eb4dc4
Sarah <sarahhale95@gmail.com>
2019-10-21 12:09:52 +0200

    fixed color on dark bg

--- src/shared/components/Moodboxes/MoodBox.js
+++ src/shared/components/Moodboxes/MoodBox.js
@@ -22,9 +22,13 @@ class MoodButton extends React.Component {
 
 
         let btn_style = this.state.clicked ? "filled-button" : "opacity-button"
+        let color = "#000000"
+        if (this.props.value === "Exciting")
+            color = "#ffffff"
+
         return (
             <button
-                style={{ backgroundColor: this.props.color }}
+                style={{ backgroundColor: this.props.color, color: color }}
                 className={"mood-box " + btn_style}
                 onClick={this.onPress.bind(this)} >
                 {this.props.value}
